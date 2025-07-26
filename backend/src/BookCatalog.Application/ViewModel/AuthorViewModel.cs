7192505acc90139da70fea6f2b6a3daf0e47adfc
helton-evambi <heltonevambi@gmail.com>
2025-07-27 00:33:11 +0100

    feat: add ViewModels classes

---
+++ backend/src/BookCatalog.Application/ViewModel/AuthorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookCatalog.Application.ViewModel;
+
+public record AuthorViewModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime? DateOfBirth { get; set; }
+    public DateTime? DateOfDeath { get; set; }
+    public string? Biography { get; set; }
+
+    public IEnumerable<BookViewModel> Books { get; set; } = new List<BookViewModel>();
+}
