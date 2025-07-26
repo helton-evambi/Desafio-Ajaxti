01911efac86b0067b04a90114b446a3b604c7238
helton-evambi <heltonevambi@gmail.com>
2025-07-26 20:27:33 +0100

    refactor: clean up PagedResult class structure and remove unnecessary internal class

--- backend/src/BookCatalog.Domain/Abstractions/PagedResult.cs
+++ backend/src/BookCatalog.Domain/Abstractions/PagedResult.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BookCatalog.Domain.Abstractions;
 
-namespace BookCatalog.Domain.Abstractions
+public class PagedResult<T>
 {
-    internal class PagedResult
-    {
-    }
+    public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < TotalPages;
+    public int FirstItemIndex => (Page - 1) * PageSize + 1;
+    public int LastItemIndex => Math.Min(Page * PageSize, TotalCount);
 }
