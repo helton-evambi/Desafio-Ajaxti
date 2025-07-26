5ba2f5e7e45c6c9b27b6294ccca60ba34582d14d
helton-evambi <heltonevambi@gmail.com>
2025-07-27 00:31:13 +0100

    feat: add detailed retrieval methods to IBookRepository

--- backend/src/BookCatalog.Domain/Interfaces/IBookRepository.cs
+++ backend/src/BookCatalog.Domain/Interfaces/IBookRepository.cs
@@ -5,4 +5,8 @@ namespace BookCatalog.Domain.Interfaces;
 
 public interface IBookRepository : IRepository<Book, Guid>
 {
+    Task<IEnumerable<PagedResult<Book>>> GetAllWithDetailsAsync(PagedParameters parameters, CancellationToken cancellationToken = default);
+    Task<Book?> GetByIdWithDetailsAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Book>> GetByAuthorIdWithDetailsAsync(Guid authorId, PagedParameters parameters, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Book>> GetByGenreIdWithDetailsAsync(Guid genreId, PagedParameters parameters, CancellationToken cancellationToken = default);
 }
