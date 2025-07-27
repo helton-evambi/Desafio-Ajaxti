0104649e89b99263f9dc7b019a13b1c8e0fd182e
helton-evambi <heltonevambi@gmail.com>
2025-07-27 16:37:24 +0100

    refactor: update IRepository interface for improved clarity and maintainability

--- backend/src/BookCatalog.Domain/Abstractions/IRepository.cs
+++ backend/src/BookCatalog.Domain/Abstractions/IRepository.cs
@@ -1,11 +1,15 @@
-﻿namespace BookCatalog.Domain.Abstractions;
+﻿using System.Linq.Expressions;
+
+namespace BookCatalog.Domain.Abstractions;
 
 public interface IRepository<T, TId>
     where T : IEntity<TId>
 {
-    Task<IEnumerable<PagedResult<T>>> GetAllAsync(PagedParameters parameters, CancellationToken cancellationToken = default);
-    Task<T?> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
-    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+    Task<IEnumerable<PagedResult<T>>> GetAllAsync(PagedParameters parameters, params Expression<Func<T, object>>[] includes);
+    Task<T?> GetByIdAsync(TId id, params Expression<Func<T, object>>[] includes);
+    Task<IEnumerable<PagedResult<T>>> FindAsync(PagedParameters parameters, Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+    Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
     Task<bool> UpdateAsync(T entity, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(TId id, CancellationToken cancellationToken = default);
 }
