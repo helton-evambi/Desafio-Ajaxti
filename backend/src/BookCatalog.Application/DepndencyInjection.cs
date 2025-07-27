c6238f315fbe83e33c3e34d6eddef9eef261f87b
helton-evambi <heltonevambi@gmail.com>
2025-07-27 17:45:21 +0100

    feat: implement dependency injection for application services

---
+++ backend/src/BookCatalog.Application/DepndencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using BookCatalog.Application.Interfaces;
+using BookCatalog.Application.Services;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BookCatalog.Application;
+
+public static class DepndencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services)
+    {
+        // validators
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        //services
+        services.AddScoped<IBookService, BookService>();
+        services.AddScoped<IAuthorService, AuthorService>();
+        services.AddScoped<IGenreSerivce, GenreService>();
+
+        return services;
+    }
+}
