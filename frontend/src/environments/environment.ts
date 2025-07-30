a4f7ff5546183266d2b9ba1c1d80b590ffa8a78c
helton-evambi <heltonevambi@gmail.com>
2025-07-30 15:41:16 +0100

    fix: update apiUrl to use HTTPS for local development

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: false,
   apiUrl: window.ENV?.API_URL
     ? window.ENV.API_URL + '/api/v1.0'
-    : 'http://localhost:7051/api/v1.0',
+    : 'https://localhost:7051/api/v1.0',
 };
