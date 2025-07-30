36d63cc2f8648caa329d542701bdd23fd4beee8e
helton-evambi <heltonevambi@gmail.com>
2025-07-30 15:40:51 +0100

    feat: add ngx-toastr for user notifications and implement toast interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import {
@@ -8,10 +10,21 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { toastInterceptor } from './core/interceptors/toast.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([errorInterceptor, toastInterceptor])
+    ),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+      progressBar: true,
+      progressAnimation: 'decreasing',
+    }),
   ],
 };
